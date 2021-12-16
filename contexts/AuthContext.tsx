0aa075515bc6708e16dc1b27b3417845b7ad778d
Paulo Santos <jwcfree11@gmail.com>
2021-12-16 17:30:51 -0300

    JWT

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,5 +1,12 @@
-import { createContext, ReactNode } from 'react'
+import { createContext, ReactNode, useState } from 'react'
 import { api } from '../services/api'
+import Router from 'next/router'
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+}
 
 type SignInCredentials = {
   email: string;
@@ -8,6 +15,7 @@ type SignInCredentials = {
 
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
+  user: User;
   isAuthenticated: boolean
 }
 
@@ -18,19 +26,26 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData)
 
 export function AuthProvider({children}: AuthProviderProps){
-  const isAuthenticated = false
+  const [user,setUser] = useState<User>()
+  const isAuthenticated = !!user
 
   async function signIn({ email, password }:SignInCredentials){
     try {
       const response = await api.post('sessions', { email, password })
-      console.log(response.data)
+      const { token, refreshToken, permissions, roles } = response.data
+      setUser({
+        email, 
+        permissions,
+        roles
+      })
+      Router.push('/dashboard')
     }catch(err) {
       console.log(err)
     }
   }
 
   return(
-    <AuthContext.Provider value={{signIn, isAuthenticated}}>
+    <AuthContext.Provider value={{signIn, isAuthenticated, user}}>
       {children}
     </AuthContext.Provider>
   )
