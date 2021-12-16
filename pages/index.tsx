9d2784eeb655aeb224acddbb16e43c16c0ddc0bf
Paulo Santos <jwcfree11@gmail.com>
2021-12-16 17:10:42 -0300

    JWT

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import styles from '../styles/Home.module.css'
+import { useContext, useState } from 'react'
+import { AuthContext } from '../contexts/AuthContext'
+
+export default function Home() {
+  const [ email, setEmail ] = useState('')
+  const [ password, setPassword ] = useState('')
+
+  const { signIn } = useContext(AuthContext)
+
+  async function handleSubmit(e){
+    e.preventDefault()
+    const data = {
+      email,
+      password
+    }
+    await signIn(data);
+    
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.container}>
+    <input type="email" value={email} onChange={e => setEmail(e.target.value)}/>
+    <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+    <button type="submit">Enviar</button>
+    </form>
+  )
+}
