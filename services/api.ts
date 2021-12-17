7ecd8bcf7b6124f3cdb55e91533d9612166b7b25
Paulo Santos <jwcfree11@gmail.com>
2021-12-17 16:08:06 -0300

    JWT

--- services/api.ts
+++ services/api.ts
@@ -1,11 +1,39 @@
-import axios from 'axios';
-import { parseCookies } from 'nookies'
+import axios, { AxiosError } from 'axios';
+import { parseCookies, setCookie } from 'nookies'
 
-const cookies = parseCookies()
+let cookies = parseCookies()
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
   headers: {
     Authorization: `Bearer ${cookies['nextAuth.token']}`
+  }
+})
+
+api.interceptors.response.use(response => {
+  return response
+}, (error: AxiosError) => {
+  if(error.response.status === 401){
+    if(error.response.data?.code === 'token.expired'){
+      cookies = parseCookies()
+      const { 'nextAuth.refreshToken': refreshToken } = cookies
+      api.post('/refresh', {
+        refreshToken
+      }).then(response => {
+        const {token} = response.data
+
+        setCookie(undefined, 'nextAuth.token', token, {
+          maxAge: 60 * 60 * 24 * 30,
+          path: '/'
+        })
+        setCookie(undefined, 'nextAuth.refreshToken', response.data.refreshToken, {
+          maxAge: 60 * 60 * 24 * 30,
+          path: '/'
+        })
+        api.defaults.headers['Authorization'] = `Bearer ${token}`
+      })
+    }else{
+
+    }
   }
 })
