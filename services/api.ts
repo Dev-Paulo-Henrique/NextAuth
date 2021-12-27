2bee4e17c97b17e018dffe1f7937e809bfdcf7bc
Paulo Santos <jwcfree11@gmail.com>
2021-12-27 14:45:14 -0300

    JWT

--- services/api.ts
+++ services/api.ts
@@ -2,6 +2,8 @@ import axios, { AxiosError } from 'axios';
 import { parseCookies, setCookie } from 'nookies'
 
 let cookies = parseCookies()
+let isRefreshing = false
+let failedRequestQueue = []
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
@@ -17,20 +19,42 @@ api.interceptors.response.use(response => {
     if(error.response.data?.code === 'token.expired'){
       cookies = parseCookies()
       const { 'nextAuth.refreshToken': refreshToken } = cookies
-      api.post('/refresh', {
-        refreshToken
-      }).then(response => {
-        const {token} = response.data
+      const originalConfig = error.config
+      if(!isRefreshing){
+        isRefreshing = true
+        api.post('/refresh', {
+          refreshToken
+        }).then(response => {
+          const {token} = response.data
+  
+          setCookie(undefined, 'nextAuth.token', token, {
+            maxAge: 60 * 60 * 24 * 30,
+            path: '/'
+          })
+          setCookie(undefined, 'nextAuth.refreshToken', response.data.refreshToken, {
+            maxAge: 60 * 60 * 24 * 30,
+            path: '/'
+          })
+          api.defaults.headers['Authorization'] = `Bearer ${token}`
 
-        setCookie(undefined, 'nextAuth.token', token, {
-          maxAge: 60 * 60 * 24 * 30,
-          path: '/'
+          failedRequestQueue.forEach(request => request.onSuccess(token))
+          failedRequestQueue = []
+        }).catch(err=>{
+          failedRequestQueue.forEach(request => request.onFailure(err))
+          failedRequestQueue = []
+        }).finally(()=>{isRefreshing = false})
+      }
+
+      return new Promise((resolve, reject) => {
+        failedRequestQueue.push({
+          onSuccess: (token: string) => {
+            originalConfig.headers['Authorization'] = `Bearer ${token}`
+            resolve(api(originalConfig))
+          },
+          onFailure: (err: AxiosError)=>{
+            reject(err)
+          }
         })
-        setCookie(undefined, 'nextAuth.refreshToken', response.data.refreshToken, {
-          maxAge: 60 * 60 * 24 * 30,
-          path: '/'
-        })
-        api.defaults.headers['Authorization'] = `Bearer ${token}`
       })
     }else{
 
