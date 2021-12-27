8e6361bfd7a078db2b7d3ac77e125ed0299f1d19
Paulo Santos <jwcfree11@gmail.com>
2021-12-27 16:32:40 -0300

    JWT

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,7 @@
-import { destroyCookie } from "nookies"
 import { useContext, useEffect } from "react"
+import { Can } from "../components/Can"
 import { AuthContext } from "../contexts/AuthContext"
-import { useCan } from "../hooks/UseCan"
+import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
 import { withSSRAuth } from "../utils/withSSRAuth"
@@ -10,9 +10,6 @@ import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard(){
   const { user } = useContext(AuthContext)
-  const userCanSeeMetrics = useCan({
-    permissions: ['metrics.list']
-  })
 
   useEffect(()=>{
     api.get('/me').then(response => console.log(response)).catch(err => console.log(err))
@@ -21,7 +18,9 @@ export default function Dashboard(){
   return (
     <>
     <h1>Dashboard: {user?.email}</h1>
-    {userCanSeeMetrics && <div>Métricas</div>}
+    <Can permissions={['metrics.list']}>
+      <div>Métricas</div>
+      </Can>
     </>
     
   )
